24bd7b6126bdcce03b35c9c3f126c20c9924600e
Harsh6k3 <96190971+Harsh6k3@users.noreply.github.com>
2025-05-06 18:55:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-x=5
-if(type(x)is int):
-    print("true")
-else:
-    print("false")
+a=10
+b=10
 
-    x=5.0
-    if (type(x)is not float):
-        print("true")
-
-    else:
-        print("false")
-
-        x=20
-        y=20
-        if(x is y):
-         print("x and y SAME identity")
-
-        y=30
-        if(x is not y):
-         print ("x and y have DIFFRENT identity")
+if a>0 and b>0:
+    print("the numbers are greater than 0")
---
+++ operator.py
@@ -0,0 +1,42 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+
+sum =tree1+tree2+tree3+tree4+tree5
+print("the sum of the trees are ",sum)
+average= sum/5
+print("the average of 5 trees are ",average)
+
+boy1=5
+boy2=6
+boy3=7
+boy4=8
+
+sum=boy1+boy2+boy3+boy4
+print("the sum of the boys are ",sum)
+average=sum/4
+print("the average of4 boys are ",average)
+
+
+
+
+
+
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v+w)*x/y
+print("value of (v+w)*x/y is ",z)
+
+
+name="Alex"
+age=0
+
+if name=="Alex" or name =="john" and age>=2:
+ print("Hello!Welcome")
+else:
+ print("Good Bye")
